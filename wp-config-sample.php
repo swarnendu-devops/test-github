d5fcb0c1fbe0b892bffe35afba43f51b4d487bc8
Swarnendu Debnath <swarnenducloudworld@gmail.com>
2020-07-17 17:42:37 +0530

    db connection change commit

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'm3idb' );
+define( 'DB_NAME', 'iim-kol' );
 
 /** MySQL database username */
 define( 'DB_USER', 'username_here' );
